e725d411c5781f05482536b9a4af3d97f4f5f82d
Виталий Травин <vtravin@nologostudio.ru>
2023-06-06 20:04:24 +0600

    js project - timer

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -34,7 +34,6 @@ const changeModalState = (state) => {
                         state[prop] = item.value;
                         break;
                 }
-                console.log(state);
             });
         });
     }
